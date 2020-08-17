931365736ecfbea7c8e1000e83377962c171502d
ASUS <samith13madushanka@gmail.com>
2020-08-17 11:41:53 +0530

    admin

---
+++ app/src/main/java/com/example/thebuger/product3.java
@@ -0,0 +1,14 @@
+package com.example.thebuger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product3);
+    }
+}
